92d490a85d05b460dd35166b3846804805c681a5
marianacaprioli <mariana.caprioli@estudante.ifms.edu.br>
2023-08-29 11:47:42 -0400

    commit

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -3,18 +3,18 @@ function Main(){
  return(
   <main>
     <h1>Seu Nome</h1>
-    <div class="portfolio">
-        <div class="project">
+    <div className="portfolio">
+        <div className="project">
             <h2>Projeto 1</h2>
             <p>Descrição do primeiro projeto.</p>
         </div>
         
-        <div class="project">
+        <div className="project">
             <h2>Projeto 2</h2>
             <p>Descrição do segundo projeto.</p>
         </div>
         
-        <div class="project">
+        <div className="project">
             <h2>Projeto 3</h2>
             <p>Descrição do terceiro projeto.</p>
         </div>
